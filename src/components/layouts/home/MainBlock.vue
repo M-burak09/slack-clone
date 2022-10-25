20fbdcc30b5d43700a5be7be5940b4dc05f210e0
Burak Kivrak <m-burak09@hotmail.com>
2022-10-25 11:34:10 +0200

    start main

--- src/components/layouts/home/MainBlock.vue
+++ src/components/layouts/home/MainBlock.vue
@@ -1,6 +1,10 @@
 <template lang="">
     <main class="bg-primary-color text-secondary-color w-full "> 
-        <h1>This is the main block</h1>
+        <div class="border-y border-quartinary-color p-4 flex">
+            <h2 clas="">Channel name</h2>
+            <div class="border rounded-lg border-quartinary-color px-4"></div>
+        </div>
+        
     </main>
 </template>
 <script>
